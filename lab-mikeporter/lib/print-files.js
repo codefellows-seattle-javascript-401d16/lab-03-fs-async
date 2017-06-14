3a24b6c8f018018091054b9be0792458faa3141c
mikeporter <porter.michael.james@gmail.com>
2017-06-14 16:59:11 -0700

    finished

--- lab-mikeporter/lib/print-files.js
+++ lab-mikeporter/lib/print-files.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // create module lib/print-files with a function that takes an array of three filepaths and a callback
 // it should have a function signature (paths, callback) => undefined
 // expect the callback function signature to be (err, data) => undefined
@@ -5,4 +7,27 @@
 // on success pass an array of the contents of the files the data param of the callback callback(null, data)
 // on failure pass and error in to the callback callback(err)
 
-function
+// A function signature (or type signature, or method signature) defines input and output of functions or methods.
+
+const printFiles = module.exports = {};
+const fs = require('fs');
+
+printFiles.readInOrder = (paths, callback) => {
+  var passedData = [];
+  fs.readFile(paths[0], (err, data) => {
+    if (err)
+      return callback(err);
+    passedData.push(data.toString());
+    fs.readFile(paths[1], (err, data) => {
+      if (err)
+        return callback(err);
+      passedData.push(data.toString());
+      fs.readFile(paths[2], (err, data) => {
+        if (err)
+          return callback(err);
+        passedData.push(data.toString());
+        callback (null, passedData);
+      });
+    });
+  });
+};
