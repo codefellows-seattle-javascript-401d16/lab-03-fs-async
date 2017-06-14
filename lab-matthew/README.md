d8bb34043825ae0a847b9780a68fbaeef1ce8dfe
Matthew Parker <cadburylion@outlook.com>
2017-06-14 15:35:54 -0700

    Cleaned out corpse code

--- lab-matthew/README.md
+++ lab-matthew/README.md
@@ -1,1 +1,7 @@
-# Lab 03 
+# print-files.js
+
+This module prints the contents of the supplied files in Node in text form.
+
+To use, in Node type: node index.js filepaths here.
+
+example: node index.js ./public/index.html ./public/about.html ./public/contact.html
--- lab-matthew/index.js
+++ lab-matthew/index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// create an index.js that calls your function with three paths from process.argv and then logs the contents of the three files to the screen
-// (your console.log should be in the index.js file not in your print-files function)
-
 const printFiles = require('./lib/print-files.js');
 
 let fileContents = module.exports = () => {
@@ -15,14 +12,3 @@ let fileContents = module.exports = () => {
 };
 
 fileContents();
-
-// let toUpper = module.exports = () => {
-//   let list = fp.splice(process.argv, [2]);
-//   list = fp.map(list, word => word.toUpperCase());
-//
-//   let result = list.join(' ');
-//   console.log(result);
-//   return result;
-// };
-//
-// toUpper();
