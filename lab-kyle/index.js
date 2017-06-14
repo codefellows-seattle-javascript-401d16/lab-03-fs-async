e94e526bf71f2bdbc71d9fc51fead4b68d147b35
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-14 14:33:39 -0700

    creates printFiles function

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const printFiles = require('./lib/print-files.js');
+
+let logPrintFiles = () => {
+  let list = process.argv.slice(2);
+  printFiles(list, (data) => {
+    console.log(data.toString());
+    return data.toString();
+  });
+};
+
+logPrintFiles();
--- lab-kyle/lib/print-files.js
+++ lab-kyle/lib/print-files.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const fs = require('fs');
+
+let printFiles = module.exports = (paths, callback) => {
+
+  fs.readFile(paths[0], (err, data) => {
+    if(err) 
+      return console.error(err);
+    callback(data);
+
+    fs.readFile(paths[1], (err, data) => {
+      if(err) 
+        return console.log(err);
+      callback(data);
+
+      fs.readFile(paths[2], (err, data) => {
+        if(err) 
+          return console.log(err);
+        callback(data);
+      });
+    });
+  });
+};
