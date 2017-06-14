017d90d83c63576c844f4b1157992abe747e4a79
Spencer Gietzen <Speng1516@comcast.net>
2017-06-14 15:12:32 -0700

    added a comment

--- lab-spencer/test/index-test.js
+++ lab-spencer/test/index-test.js
@@ -11,6 +11,7 @@ describe('File-Reader', () => {
         console.error(err);
         return err;
       }
+      // this is the output of those three files in the right order
       expect(data).toEqual(['HEY THIS IS 1\ns\nd1as\ndsa\nd\n1\n 11111111111111111\n\n 111\n', '\'use strict\';\n\nconsole.log(\'This is 2!\');\nif(two) {\n  two();\n  console.error(\'This is not an error, it is 2!!!!!!!!!!\');\n}\n\nfunction two() {\n  console.log(\'2 yo\');\n}\n', '<html3>\n  <head3>\n    <title3>THis is 3</title3>\n  </head3>\n  <body3>\n    <p3>Hey this is 3</p>\n  </body3>\n</html>\n']);
       done();
     });
