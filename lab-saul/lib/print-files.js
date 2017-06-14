7bcd7941458bab87c343d8ff238cf3ddb70c15a2
Saul Greene <spdysg@gmail.com>
2017-06-14 16:19:58 -0700

    mvp

--- lab-saul/lib/print-files.js
+++ lab-saul/lib/print-files.js
@@ -1,34 +1,33 @@
 'use strict';
 
-// const Buffer = require('buffer').Buffer;
+
 const fs = require('fs');
-const paths  = ['../data/data.txt', '../data/data1.txt', '../data/data2.txt'];
 
-//module.exports =
 
-const that = module.exports = function (paths){
+const that = module.exports = function (paths, callback){
   let hexArray = [];
-
   fs.readFile(paths[0], function(err,data){
-    if(err) throw err;
+    if(err) console.error(err);
 
     let firstBuffer = data.toString('utf8', 0);
     hexArray.push(firstBuffer);
 
     fs.readFile(paths[1], function(err,data){
-      if(err) throw err;
+      if (err) throw err;
 
       let secondBuffer = data.toString('utf8', 0);
       hexArray.push(secondBuffer);
 
 
       fs.readFile(paths[2], function(err,data){
-        if(err) throw err;
+        if(err) return callback(err);
 
         let thirdBuffer = data.toString('utf8', 0);
         hexArray.push(thirdBuffer);
         console.log(hexArray);
-        return hexArray;
+
+        // call the callback with null, hexArray
+        // callback(null, hexArray);
       });
     });
   });
