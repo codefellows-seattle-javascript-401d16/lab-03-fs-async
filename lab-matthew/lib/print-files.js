6014328c0ef66565d890407870cae01d2a4d655e
Matthew Parker <cadburylion@outlook.com>
2017-06-14 16:15:18 -0700

    Cleaned out commented out code

--- lab-matthew/lib/print-files.js
+++ lab-matthew/lib/print-files.js
@@ -2,15 +2,6 @@
 
 const fs = require('fs');
 
-// create module lib/print-files with a function that takes an array of three filepaths and a callback
-// it should have a function signature (paths, callback) => undefined
-// expect the callback function signature to be (err, data) => undefined
-// read the three files in order
-// on success pass an array of the contents of the files the data param of the callback callback(null, data)
-// on failure pass and error in to the callback callback(err)
-// create an index.js that calls your function with three paths from process.argv and then logs the contents of the three files to the screen
-// (your console.log should be in the index.js file not in your print-files function)
-
 module.exports = (paths, callback) => {
 
   let outputs = [];
