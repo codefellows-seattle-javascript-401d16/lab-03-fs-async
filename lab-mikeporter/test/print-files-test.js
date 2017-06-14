005f50daf54fd275d9bc4cfb2b5d237ba833d481
mikeporter <porter.michael.james@gmail.com>
2017-06-14 14:04:58 -0700

    added todos

---
+++ lab-mikeporter/test/print-files-test.js
@@ -0,0 +1,3 @@
+// Write tests that guarantee that the files are logged in order.
+// You will also need to look up and implement async testing, making sure that you are not getting "false positives" (tests that are passing but shouldn't be).
+// hint read mocha docs
