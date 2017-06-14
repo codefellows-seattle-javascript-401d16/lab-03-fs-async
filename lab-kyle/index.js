d0a94f5dc100e7070f3ffc7ac6b194e64177762e
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-14 14:53:06 -0700

    refactored err logs into index.js

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -4,9 +4,14 @@ const printFiles = require('./lib/print-files.js');
 
 let logPrintFiles = () => {
   let list = process.argv.slice(2);
-  printFiles(list, (data) => {
-    console.log(data.toString());
-    return data.toString();
+  printFiles(list, (err, data) => {
+    if(err) {
+      console.log(err);
+      return err;
+    } else {
+      console.log(data.toString());
+      return data.toString();
+    }
   });
 };
 
--- lab-kyle/lib/print-files.js
+++ lab-kyle/lib/print-files.js
@@ -5,19 +5,13 @@ const fs = require('fs');
 let printFiles = module.exports = (paths, callback) => {
 
   fs.readFile(paths[0], (err, data) => {
-    if(err) 
-      return console.error(err);
-    callback(data);
+    callback(err, data);
 
     fs.readFile(paths[1], (err, data) => {
-      if(err) 
-        return console.log(err);
-      callback(data);
+      callback(err, data);
 
       fs.readFile(paths[2], (err, data) => {
-        if(err) 
-          return console.log(err);
-        callback(data);
+        callback(err, data);
       });
     });
   });
