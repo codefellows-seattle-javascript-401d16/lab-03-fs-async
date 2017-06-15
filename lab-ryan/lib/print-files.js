1d4bd366c56e34604855df962e9783ca4e092285
Ryan Turner <ryanthepit@gmail.com>
2017-06-14 19:17:07 -0700

    created function w 3 file paths and a callback

--- lab-ryan/lib/print-files.js
+++ lab-ryan/lib/print-files.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const fs = require('fs');
+
+module.exports = (paths, callback) => {
+  let results = [];
+  fs.readFile(paths[0], (err, data) => {
+    if (err) return callback(err);
+    results.push(data.toString());
+
+    fs.readFile(paths[1], (err, data) => {
+      if (err) return callback(err);
+      results.push(data.toString());
+
+      fs.readFile(paths[2], (err, data) => {
+        if (err) return callback(err);
+        results.push(data.toString());
+        callback(null, results);
+      });
+    });
+  });
+};
