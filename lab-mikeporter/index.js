4958e237449e645961169fdf6ae1734272c9ea5e
mikeporter <porter.michael.james@gmail.com>
2017-06-14 18:00:13 -0700

    finished index test

--- lab-mikeporter/index.js
+++ lab-mikeporter/index.js
@@ -1,7 +1,7 @@
+'use strict';
+
 // create an index.js that calls your function with three paths from process.argv and then logs the contents of the three files to the screen
 // (your console.log should be in the index.js file not in your print-files function)
-
-'use strict';
 
 const printFiles = require('./lib/print-files.js');
 
--- lab-mikeporter/test/print-files-test.js
+++ lab-mikeporter/test/print-files-test.js
@@ -1,3 +1,33 @@
+'use strict';
+
 // Write tests that guarantee that the files are logged in order.
 // You will also need to look up and implement async testing, making sure that you are not getting "false positives" (tests that are passing but shouldn't be).
 // hint read mocha docs
+
+const printFiles = require('../lib/print-files.js');
+const expect = require('expect');
+
+describe('printFiles.js', () => {
+  describe('prints files in order', () => {
+    let oldArgv = [];
+    before(() => {
+      oldArgv = process.argv;
+      process.argv = ['node', 'index', './to-read/first.js', './to-read/second.js', './to-read/third.js'];
+    });
+    after(() => {
+      process.argv = oldArgv;
+    });
+    it('expect to be in order', (done) => {
+      let proc = process.argv.slice(2);
+      printFiles.readInOrder(proc, (err, data) => {
+        if (err) {
+          console.error(err);
+          return err;
+        }
+        expect(data)
+          .toEqual([ 'first file to read\n', 'second file to read\n', 'third file to read\n' ]);
+        done();
+      });
+    });
+  });
+});
