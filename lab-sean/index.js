d8efe742681e3b6f8922214b22fb3a59ddb9d824
Sean Reigle <sereigle@gmail.com>
2017-06-14 22:40:06 -0700

    added JS and files

---
+++ lab-sean/index.js
@@ -0,0 +1,10 @@
+'use-strict';
+
+const printFile = require('./lib/print-files.js');
+
+printFile(process.argv.slice(2), (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(data.toString());
+});
