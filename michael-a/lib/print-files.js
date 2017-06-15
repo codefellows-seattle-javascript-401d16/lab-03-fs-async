9445fc124456bec245e5e4ff3bdd74116401fa2e
Michael Axelson <axelmichael11@gmail.com>
2017-06-15 11:20:26 -0700

    changin up the structure of my functions, added the test file for fun, this is duncans code dont count the test pleasee

--- michael-a/lib/print-files.js
+++ michael-a/lib/print-files.js
@@ -1,31 +1,28 @@
+'use strict';
+
 const fs = require('fs');
 
 
 
-const fp = module.exports = (paths, callback) => {
+module.exports = (paths, callback) => {
   let files = [];
   paths = ['./file1.txt','./file2.txt','./file3.txt'];
   fs.readFile(paths[0], (err,data) => {
     if (err)
-      return console.log(err);
-    console.log(data.toString());
+      return callback(err);
     files.push(data.toString());
 
     fs.readFile(paths[1], (err,data) => {
       if (err)
-        return console.log(err);
-      console.log(data.toString());
+        return callback(err);
       files.push(data.toString());
 
       fs.readFile(paths[2], (err,data) => {
         if (err)
-          return console.log(err);
-        console.log(data.toString());
+          return callback(err);
         files.push(data.toString());
         callback(null, files);
       });
     });
   });
 };
-
-fp();
