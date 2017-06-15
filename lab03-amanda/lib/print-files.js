3afc00bfb8a90565ad3df40707b7d45ac8dd13dd
Amanda Koster <amandakoster@gmail.com>
2017-06-14 17:16:13 -0700

    argv works. Lab complete

--- lab03-amanda/lib/print-files.js
+++ lab03-amanda/lib/print-files.js
@@ -1,35 +1,31 @@
 'use strict';
 
-// create module lib/print-files with a function that takes an array of three filepaths and a callback
-// it should have a function signature (paths, callback) => undefined
-// expect the callback function signature to be (err, data) => undefined
-// read the three files in order
-// on success pass an array of the contents of the files the data param of the callback callback(null, data)
-// on failure pass and error in to the callback callback(err)
-
 const fs = require('fs');
-let arrayPaths = ['./file1.html','./file2.html', './file3.html'];
 
 const printFiles = module.exports = {};
-printFiles.readFile = function() {
+
+printFiles.readFile = function(arrayPaths, callback) {
+
+  let dataArray = [];
+
   fs.readFile(arrayPaths[0],(err, data) => {
+    console.log(arrayPaths);
     if(err)
       throw(err);
-    console.log(data);
+    dataArray.push(data.toString());
 
     fs.readFile(arrayPaths[1],(err, data) => {
       if(err)
         throw(err);
-      console.log(data);
+      dataArray.push(data.toString());
 
-      fs.readFile(arrayPaths[3],(err, data) => {
+      fs.readFile(arrayPaths[2],(err, data) => {
         if(err)
           throw(err);
-        console.log(data);
+        dataArray.push(data.toString());
 
+        callback(null, dataArray);
       });
     });
   });
 };
-
-printFiles.readFile();
