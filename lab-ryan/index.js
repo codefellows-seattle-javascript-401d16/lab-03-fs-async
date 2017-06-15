941c8b7e1fd3660ec7cc35d71aa2fece9ef9c23f
Ryan Turner <ryanthepit@gmail.com>
2017-06-14 19:24:08 -0700

    fixed index file

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const printFiles = require('./lib/print-files.js');
+
+let contents = module.exports = () => {
+  let results = printFiles(process.argv.slice(2), (err, data) => {
+    data.forEach((file) => {
+      console.log(file);
+    });
+  });
+  return results;
+};
+contents();
