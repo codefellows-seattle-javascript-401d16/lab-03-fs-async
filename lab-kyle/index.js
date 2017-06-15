f587f778dcfada5d6f3d889ac7a0a659ff6398d9
Kyle Aardal <aardal.kyle@gmail.com>
2017-06-14 17:56:07 -0700

    print-file now returns an array

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -2,15 +2,17 @@
 
 const printFiles = require('./lib/print-files.js');
 
-let logPrintFiles = () => {
+let logPrintFiles = module.exports = () => {
   let list = process.argv.slice(2);
   printFiles(list, (err, data) => {
     if(err) {
       console.log(err);
       return err;
     } else {
-      console.log(data.toString());
-      return data.toString();
+      let results = data.map(item => item.toString());
+      results = results.join(' ');
+      console.log(results);
+      return results;
     }
   });
 };
