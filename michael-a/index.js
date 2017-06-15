fc56977a3901b2c5e3f1af922e74081676b1f32d
Michael Axelson <axelmichael11@gmail.com>
2017-06-15 07:36:10 -0700

    print files almost there... as of right now I am pushing file contents to an array...

---
+++ michael-a/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+const fs = require('fs');
+
+
+fs.readFile('./fs-demo.js', (err, data) => {
+  if(err)
+    return console.error(err);
+  console.log('booyea', data.toString());
+
+  fs.readFile('./data/0.txt', (err, data) => {
+    if(err)
+      return console.log(err);
+    console.log('sweeet', data.toString());
+  });
+});
