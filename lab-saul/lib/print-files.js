445d297ba3632389588ec16ac45cb94a8dcfaeb6
Saul Greene <spdysg@gmail.com>
2017-06-14 19:00:44 -0700

    mvp

--- lab-saul/lib/print-files.js
+++ lab-saul/lib/print-files.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const that = module.exports = function (paths, callback){
   let hexArray = [];
   fs.readFile(paths[0], function(err,data){
-    if(err) console.error(err);
+    if(err) throw err;
 
     let firstBuffer = data.toString('utf8', 0);
     hexArray.push(firstBuffer);
--- lab-saul/test/test.js
+++ lab-saul/test/test.js
@@ -1,1 +1,18 @@
 'use strict';
+
+//this is not working yet
+const reader = require('../lib/print-files.js');
+const expect = require('expect');
+
+let filePaths =['../data/data.txt',`../data/data1.txt`,`../data/data2.txt`];
+let bufferData = [];
+
+bufferData = reader(filePaths);
+
+describe('index.js', function(){
+  describe('hexArray', function(){
+    it('the zero index should be the same as the data from first data file', function(){
+      expect(bufferData[0],'WHATHERE').equal(['this is data text number one\n' ]);
+    });
+  });
+});
