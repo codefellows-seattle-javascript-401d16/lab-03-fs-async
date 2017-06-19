72bd613b9f2366f43efb92c4b86f61ebff476cff
Joshua Evans <skyfriends1@gmail.com>
2017-06-18 18:53:21 -0700

    fixed test and can print if you put in command line with 3 path names

--- lab-joshuaevans/test/print-files-test.js
+++ lab-joshuaevans/test/print-files-test.js
@@ -2,18 +2,15 @@
 
 let expect = require('expect');
 let pf = require('../lib/print-files.js');
-let fs = require('fs');
 let samplePaths = ['../data/1.txt', '../data/2.txt', '../data/3.txt'];
 
 describe('testing to see if the files are printing in order', () => {
   it('should print all files in order', () => {
-    pf(samplePaths[0], data)
-    expect(data).toEqual('this is the first file')
+    pf(samplePaths, (err, data) => {
+      if (err) {
+        return err;
+      }
+      expect(data).toEqual('this is the first file');
+    });
   });
-//   it('should print the second file', () => {
-//     expect()
-//   });
-//   it('should print the third file', () => {
-//
-//   })
 });
