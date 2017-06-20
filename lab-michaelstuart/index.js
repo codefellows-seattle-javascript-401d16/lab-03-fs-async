d13a37730aa62370714bf5f13c94f1ed6ade603e
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-19 20:33:42 -0700

    refactor

--- lab-michaelstuart/index.js
+++ lab-michaelstuart/index.js
@@ -1,9 +1,12 @@
-const print = require('./lib/print-files')
+const print = require('./lib/print-files');
 
-let args = process.argv.slice(2);
+let args = process.argv.slice(2, 5);
 
-// args = ['./file1.txt', './file2.txt', './file3.txt']
+if (args.length !== 3) {
+  args = ['./file1.txt', './file2.txt', './file3.txt'];
+}
 
-const callback = arr => arr.forEach(v => console.log(v));
-
-print(args, callback);
+print(args, (data, err) => {
+  if (err) return console.log(err);
+  data.forEach(v => console.log(v.toString()));
+});
--- lab-michaelstuart/lib/print-files.js
+++ lab-michaelstuart/lib/print-files.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 module.exports = (paths, cb) =>
-  fs.readFile(paths[0], (err, data1) =>
-    fs.readFile(paths[1], (err, data2) =>
-      fs.readFile(paths[2], (err, data3) =>
-        cb([data1, data2, data3].map(v => v.toString())))));
+  fs.readFile(paths[0], (err1, data1) =>
+    fs.readFile(paths[1], (err2, data2) =>
+      fs.readFile(paths[2], (err3, data3) =>
+        cb([data1, data2, data3], (err1 || err2 || err3)))));
