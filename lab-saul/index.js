cca854a0d756e33525c8b852d80e34f0c3d1172f
Saul Greene <spdysg@gmail.com>
2017-06-25 14:21:47 -0700

    still need to make eslint work

--- lab-saul/index.js
+++ lab-saul/index.js
@@ -2,6 +2,7 @@
 
 const fileRead = require('./lib/print-files.js')
 
+
 let main = module.exports = () => {
   let files = process.argv.slice(2);
   fileRead(files, (err, data) => {
--- lab-saul/test/test.js
+++ lab-saul/test/test.js
@@ -1,10 +1,10 @@
 'use strict';
 
-//this is not working yet
+//this is not working yet, but is extra credit so wait
 const reader = require('../lib/print-files.js');
 const expect = require('expect');
 
-let filePaths =['../data/data.txt',`../data/data1.txt`,`../data/data2.txt`];
+let filePaths =['../data/data.txt','../data/data1.txt','../data/data2.txt'];
 let bufferData = [];
 
 bufferData = reader(filePaths);
@@ -12,7 +12,7 @@ bufferData = reader(filePaths);
 describe('index.js', function(){
   describe('hexArray', function(){
     it('the zero index should be the same as the data from first data file', function(){
-      expect(bufferData[0],'WHATHERE').equal(['this is data text number one\n' ]);
+      expect(bufferData[0]).equal(['this is data text number one\n' ]);
     });
   });
 });
